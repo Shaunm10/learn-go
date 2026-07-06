b99ba296ffbf5020b6cdb8d8684894add3cbba5b
shaunsargent <ShaunM10@gmail.com>
2026-07-06 09:09:07 -0400

    add launch and task configurations for investment calculator; create initial Go module and binary

--- 1-basic/app.go
+++ 1-basic/app.go
@@ -1,5 +1,8 @@
+// this is a code organization thing. much like
+// "namespaces" or "modules" in other languages.
 package main
 
+// "format" standard library included in Go.
 import "fmt"
 
 func main() {
