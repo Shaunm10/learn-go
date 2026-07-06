cfb90885e9228c845da99c7fdb117ca9e0ccecea
shaunsargent <ShaunM10@gmail.com>
2026-07-06 11:41:25 -0400

    - finished the investment calculator

--- 2-essentials/investment-calculator/investment_calculator.go
+++ 2-essentials/investment-calculator/investment_calculator.go
@@ -16,12 +16,31 @@ Future Value -> 17081.4
 Future Value -> (adjusted for Inflation): 10446.2
 */
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+
+	const inflationRate = 2.5
+	// this is an example, however not very readable, so this should be split up.
+	var investmentAmount, years, expectedReturnRate = 1000.0, 10.0, 5.5
+	////expectedReturnRate := 5.5
+	//years := 10
+
+	// prompt for Investment Amount
+	fmt.Print("What is the investment amount:")
+	// populates the variable (via pointer) to the inputted value
+	fmt.Scan(&investmentAmount)
+
+	// prompt for number of years
+	fmt.Print("How many years is the investment for:")
+	fmt.Scan(&years)
+
+	// prompt for expected return rate
+	fmt.Print("What is the expected return rate:")
+	fmt.Scan(&expectedReturnRate)
 
 	var percentageIncrease = (1 + expectedReturnRate/100)
-	var futureValue = float64(investmentAmount) * math.Pow((percentageIncrease), float64(years))
+	var futureValue = investmentAmount * math.Pow((percentageIncrease), years)
+	var futureValueAdjustedForInflation = futureValue / math.Pow((1+(inflationRate/100)), years)
 
-	fmt.Println("The future value of the investment is: " + strconv.FormatFloat(futureValue, 'f', -1, 64))
+	fmt.Println("The future value of the investment is: " + strconv.FormatFloat(futureValue, 'f', 2, 64))
+	fmt.Println("The future value of the investment factoring inflation is: " + strconv.FormatFloat(futureValueAdjustedForInflation, 'f', 2, 64))
+
 }
