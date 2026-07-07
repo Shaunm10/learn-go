192b67ff895bfae35f4203cfbdfb89030cd79cc6
shaunsargent <ShaunM10@gmail.com>
2026-07-07 10:52:52 -0400

    Adding different formatting examples

--- 2-essentials/profit-calculator/profit-calculator.go
+++ 2-essentials/profit-calculator/profit-calculator.go
@@ -9,9 +9,13 @@ func main() {
 
 	revenue, expenses, taxRate := 0.0, 0.0, 0.0
 
-	fmt.Println("===============================================")
-	fmt.Println("Welcome to the profit calculator")
-	fmt.Println("===============================================")
+	fmt.Println(`
+		===============================================
+		Welcome to the profit calculator
+
+		Please enter your revenue, expenses, and tax rate to calculate your profit.
+		We will calculate your earnings before tax, earnings after tax, and the ratio of revenue to profit.
+===============================================`)
 
 	fmt.Println("")
 	fmt.Println("")
@@ -38,7 +42,13 @@ func main() {
 	ratio := ebt / profit
 
 	// output
-	fmt.Println("Your Earnings before Tax: $" + strconv.FormatFloat(ebt, 'f', 2, 64))
-	fmt.Println("Your Earnings after Tax: $" + strconv.FormatFloat(earningsAfterTax, 'f', 2, 64))
+	//fmt.Println("Your Earnings before Tax: $", ebt)
+	fmt.Printf("Your Earnings before Tax: $%v \n\n", ebt)
+	//fmt.Println("Your Earnings after Tax: $" + strconv.FormatFloat(earningsAfterTax, 'f', 2, 64))
+	//fmt.Printf("Your Earnings after Tax: $%v \n", strconv.FormatFloat(earningsAfterTax, 'f', 2, 64))
+	fmt.Printf("Your Earnings after Tax: $%.2f \n", earningsAfterTax)
+
 	fmt.Println("Your ratio of revenue to profit is: %" + strconv.FormatFloat(ratio, 'f', 2, 64))
+
+	fmt.Println(``)
 }
