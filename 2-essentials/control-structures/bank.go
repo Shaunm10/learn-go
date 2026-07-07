bf2963e934c2d9b1cbec4e824e6f499ca88cc653
shaunsargent <ShaunM10@gmail.com>
2026-07-07 14:53:43 -0400

    more setup.

---
+++ 2-essentials/control-structures/bank.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+	GOALS:
+	=================
+	- Ask the user what operation they would like to perform in our bank application.
+	"
+	Welcome to Go Bank!
+	What do you want to do?
+	1. Check Balance
+	2. Deposit money
+	3. Withdraw money
+	4. Exit
+	Your choice:
+	"
+*/
+
+func main() {
+
+}
