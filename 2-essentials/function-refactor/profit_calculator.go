b51c2ab006e931800eb2b51ab404f9b68fa950c0
shaunsargent <ShaunM10@gmail.com>
2026-07-07 11:23:33 -0400

    finished exercise

--- 2-essentials/function-refactor/profit_calculator.go
+++ 2-essentials/function-refactor/profit_calculator.go
@@ -2,25 +2,54 @@ package main
 
 import "fmt"
 
+/*
+	GOALS:
+	- refactor the prompt for input into different functions.
+	- refactor the calculation of variables into a different function.
+*/
+
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	//var revenue float64
+	//var expenses float64
+	//var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	revenue := promptForFloatInput("Revenue:")
+	// fmt.Print("Revenue: ")
+	// fmt.Scan(&revenue)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	expenses := promptForFloatInput("Expenses:")
+	// fmt.Print("Expenses: ")
+	// fmt.Scan(&expenses)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	// fmt.Print("Tax Rate: ")
+	// fmt.Scan(&taxRate)
+	taxRate := promptForFloatInput("Tax Rate:")
 
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := performCalculations(revenue, expenses, taxRate)
+	// ebt := revenue - expenses
+	// profit := ebt * (1 - taxRate/100)
+	// ratio := ebt / profit
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+	// fmt.Println(ebt)
+	// fmt.Println(profit)
+	// fmt.Println(ratio)
+	fmt.Printf(`
+Financial Results:
+	EBT: $%.2f
+	Profit: $%.2f
+	Ratio: %.6f
+	`, ebt, profit, ratio)
+}
+
+func promptForFloatInput(prompt string) (capturedValue float64) {
+	fmt.Print(prompt)
+	fmt.Scan(&capturedValue)
+	return
+}
+
+func performCalculations(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return
 }
