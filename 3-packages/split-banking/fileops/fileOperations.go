43deb2c0d502e804617999d775a27cec7a409940
shaunsargent <ShaunM10@gmail.com>
2026-07-08 19:48:34 -0400

    - added importing a file example

---
+++ 3-packages/split-banking/fileops/fileOperations.go
@@ -0,0 +1,30 @@
+package fileops
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func ReadFloatFromFile(fileName string, defaultValue float64) (float64, error) {
+	data, err := os.ReadFile(fileName)
+
+	if err != nil {
+		return defaultValue, errors.New("Failed to find file.")
+	}
+
+	valueText := string(data)
+	value, err := strconv.ParseFloat(valueText, 64)
+
+	if err != nil {
+		return defaultValue, errors.New("Failed to parse stored value.")
+	}
+
+	return value, nil
+}
+
+func WriteFloatToFile(value float64, filePath string) {
+	valueText := fmt.Sprint(value)
+	os.WriteFile(filePath, []byte(valueText), 0644)
+}
