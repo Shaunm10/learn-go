4ebe38cdc3dcec11a2a650893d0ebe570da87f00
shaunsargent <ShaunM10@gmail.com>
2026-07-19 09:02:55 -0400

    added Generics example

--- 6-interfaces/note-taker/main.go
+++ 6-interfaces/note-taker/main.go
@@ -10,7 +10,27 @@ import (
 	"example.com/note/todo"
 )
 
+// in Go it's common to name the interface based on the method IF
+// the interface only has 1 method, so {methodName} + "er"
+// same rules apply for private vs public being camelCase or PascalCase
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+type outputtable interface {
+	// embedded from another interface
+	saver
+
+	// new function definition
+	Display()
+}
+
 func main() {
+
 	title, content := getNoteData()
 
 	userNote, err := note.New(title, content)
@@ -20,29 +40,32 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
-
-	if err != nil {
-		fmt.Println("Saving the note failed.")
-		return
-	}
-	fmt.Println("Saving the note succeeded!")
+	outputData(userNote)
 
 	todoText := getUserInput("Todo Text: ")
 
 	myTodo, err := todo.New(todoText)
 
-	if err == nil {
-		fmt.Println("todo saved!")
-	}
+	outputData(myTodo)
+}
 
-	err = myTodo.Save()
+func saveData(note saver) error {
+	err := note.Save()
+
 	if err != nil {
-
-		myTodo.Display()
+		fmt.Println("Saving the note failed.")
+		return err
 	}
+	fmt.Println("Saving the note succeeded!")
+	return nil
+}
 
+func outputData(data outputtable) {
+
+	err := data.Save()
+	if err == nil {
+		data.Display()
+	}
 }
 
 func getNoteData() (string, string) {
@@ -67,4 +90,43 @@ func getUserInput(prompt string) string {
 	text = strings.TrimSuffix(text, "\r")
 
 	return text
+}
+
+func printSomething(value interface{}) {
+
+	// see if this value is an int
+	typedVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("This is an int: ", typedVal)
+	}
+
+	// NOTE: in these cases, the value's type doesn't get "narrowed" like in
+	// TypeScript's type guards or use of typeof
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer:", value)
+	case float64:
+		fmt.Println("Float:", value)
+	case string:
+		fmt.Println("string:", value)
+	default:
+		// so some fallback logic
+
+	}
+}
+
+// 'T' is the generic type, and since it's 'any' really
+// any type can be passed in, but they must match. This will not compile
+// because not every type can support the addition/ concatenation operator.
+// func addAny[T any](a, b T) T {
+
+// 	// won't compile
+// 	return a + b
+// }
+
+// only int's float64 and strings can be passed,
+// however the types for the parameters have to match
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
