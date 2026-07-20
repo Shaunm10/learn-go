dccf2bc00914dbbdb11cc4a6d3246af617aad6c6
shaunsargent <ShaunM10@gmail.com>
2026-07-20 11:31:25 -0400

    Added array exercise

---
+++ 7-collections/array-exercise/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	title string
+	price float64
+	id    int
+}
+
+func main() {
+	// Time to practice what you learned!
+	fmt.Println("ready to go....")
+
+	// 1) Create a new array (!) that contains three hobbies you have
+	hobbies := [3]string{"play guitar", "run", "play video games"}
+	// 		Output (print) that array in the command line.
+	fmt.Println(hobbies)
+
+	// 2) Also output more data about that array:
+	//		- The first element (standalone)
+	fmt.Println(hobbies[0])
+	//		- The second and third element combined as a new list
+	fmt.Println(hobbies[1:])
+
+	// 3) Create a slice based on the first element that contains
+	//		the first and second elements.
+	slice1 := hobbies[:2]
+	//		Create that slice in two different ways (i.e. create two slices in the end)
+	slice2 := hobbies[0:2]
+
+	//fmt.Println("slice1:", slice1)
+	fmt.Println("slice2:", slice2)
+
+	// 4) Re-slice the slice from (3) and change it to contain the second
+	//		and last element of the original array.
+	slice1 = hobbies[1:]
+	fmt.Println("slice1:", slice1)
+
+	// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
+	courseGoals := []string{"learn go", "build web Api's in go"}
+	fmt.Println("Goals: ", courseGoals)
+
+	// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
+	courseGoals[1] = "Build a Rest Service in Go"
+	courseGoals = append(courseGoals, "Add Go to resume")
+
+	// 7) Bonus: Create a "Product" struct with title, id, price and create a
+	//		dynamic list of products (at least 2 products).
+	//		Then add a third product to the existing list of products.
+	productList := []Product{
+		Product{
+			title: "Fender Stratocaster",
+			price: 999.99,
+			id:    1,
+		},
+		Product{
+			title: "Nintendo Switch 2",
+			price: 499.99,
+			id:    2,
+		},
+	}
+	productList = append(productList, Product{
+		title: "Weber Grill",
+		price: 899.99,
+		id:    2,
+	})
+
+	fmt.Println(productList)
+	fmt.Println("done!")
+}
--- 7-collections/lists.go
+++ 7-collections/lists.go
@@ -2,7 +2,45 @@ package main
 
 import "fmt"
 
+type Product struct {
+	title string
+	id    string
+	price float64
+}
+
 func main() {
-	fmt.Println("ready to start with collections.")
+	// creates a fixed sized array of float64 with 4 values.
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+
+	// set the first item
+	prices[0] = 99.9
+
+	//thirdElement := prices[2]
+
+	//lastTwoPrices := prices[2:4]
+
+	// gets all the items up to the 3rd one.
+	beginningPrices := prices[:3]
+
+	// gets all the items index 2 and beyond
+	lastPrices := prices[2:]
+
+	numberOfItems := len(lastPrices)
+
+	// dynamically sized array
+	pricesOfAnySize := []float64{}
+	// or initialized with values
+	pricesOfAnySize2 := []float64{10.99, 79.99}
+
+	// this will crash at runtime because this array doesn't yet have this index.
+	pricesOfAnySize[2] = 10.99
+
+	// append creates a new array but copying the existing AND adding 1 more item to the end.
+	updatedPrices := append(pricesOfAnySize, 69.99)
+
+	// removing the first element in the array is done via a slice
+	pricesOfAnySize = pricesOfAnySize[1:]
+
+	fmt.Println(updatedPrices)
 
 }
