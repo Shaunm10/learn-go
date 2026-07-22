f6f2d224b207d5fac7e3ca1fbee21ebccc53e38e
shaunsargent <ShaunM10@gmail.com>
2026-07-22 10:05:46 -0400

    Added ability to load prices from flat file.

--- 9-tax calculator/main.go
+++ 9-tax calculator/main.go
@@ -5,11 +5,12 @@ import (
 )
 
 func main() {
-	//prices := []float64{10, 20, 30}
+
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
 		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+
 		priceJob.Process()
 	}
 }
--- 9-tax calculator/prices/prices.go
+++ 9-tax calculator/prices/prices.go
@@ -1,6 +1,13 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const pricesFilePath = "prices.txt"
 
 type TaxIncludedPriceJob struct {
 	TaxRate     float64
@@ -10,20 +17,73 @@ type TaxIncludedPriceJob struct {
 }
 
 func (taxIncludedPriceJob *TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+	result := make(map[string]string)
+	taxIncludedPriceJob.loadData()
 
 	for _, price := range taxIncludedPriceJob.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + taxIncludedPriceJob.TaxRate)
+		// perform calculation
+		taxIncludedPrice := price * (1 + taxIncludedPriceJob.TaxRate)
+
+		// set the value formatted to 2 decimal points
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 	fmt.Println(result)
 	//taxIncludedPriceJob.TaxIncludedPrices = result
 }
 
+func (taxIncludedPriceJob *TaxIncludedPriceJob) loadData() {
+
+	file, err := os.Open(pricesFilePath)
+
+	if err != nil {
+		fmt.Println("Error opening price.txt", err)
+		// TODO: handle gracefully
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var linesBuffer []string
+	for scanner.Scan() {
+		linesBuffer = append(linesBuffer, scanner.Text())
+	}
+
+	// did the scanner have an error?
+	err = scanner.Err()
+	if err != nil {
+		// TODO: handle gracefully
+		fmt.Println("Error scanning file", err)
+		file.Close()
+		return
+	}
+	file.Close()
+
+	prices := make([]float64, len(linesBuffer))
+	// iterate thought the strings converting them.
+	for lineIndex, line := range linesBuffer {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			// if one price conversion failed, stop processing
+			fmt.Printf("Unable to convert value '%v' to a valid Float64 \n\n", line)
+			fmt.Println(err)
+			file.Close()
+
+			return
+		} else {
+
+			prices[lineIndex] = floatPrice
+			//prices = append(prices, floatPrice)
+		}
+	}
+	// finally populate the input prices
+	taxIncludedPriceJob.InputPrices = append(taxIncludedPriceJob.InputPrices, prices...)
+}
+
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
-	defaultInputPrices := []float64{10, 20, 30}
+	//defaultInputPrices := []float64{10, 20, 30}
 
 	return &TaxIncludedPriceJob{
-		TaxRate:     taxRate,
-		InputPrices: defaultInputPrices,
+		TaxRate: taxRate,
+		//InputPrices: defaultInputPrices,
 	}
 }
