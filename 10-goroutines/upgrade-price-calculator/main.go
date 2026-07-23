40f0f89faa072fe2f72a8459a7506cc257d06ff9
shaunsargent <ShaunM10@gmail.com>
2026-07-23 10:50:23 -0400

    Added go routines in a loop

--- 10-goroutines/upgrade-price-calculator/main.go
+++ 10-goroutines/upgrade-price-calculator/main.go
@@ -10,15 +10,28 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+	// create an "slice" of channels with the size of our loop
+	doneChans := make([]chan bool, len(taxRates))
+	for index, taxRate := range taxRates {
+
+		// for each iteration create a new channel and set it to the index
+		doneChans[index] = make(chan bool)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("./output/result_%.0f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		err := priceJob.Process()
 
-		if err != nil {
-			fmt.Println("Could not process job")
-			fmt.Println(err)
-		}
+		go priceJob.Process(doneChans[index])
+
+		// if err != nil {
+		// 	fmt.Println("Could not process job")
+		// 	fmt.Println(err)
+		// }
 	}
+
+	// now wait for each go routine to finish
+	for _, doneChan := range doneChans {
+		<-doneChan
+	}
+
+	fmt.Println("Complete!")
 }
